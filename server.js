d6bf3e0fe2fd3650cd28c928c40282940e53c44e
MeshachB <MeshachCBrown@gmail.com>
2025-12-07 21:40:08 -0500

    Connected to MongoDB, installed mongoose, added .env setup, and updated server.js

--- server.js
+++ server.js
@@ -1,10 +1,25 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose'); 
+require('dotenv').config();
+
+
+
+
+mongoose.connect(process.env.MONGODB_URI)
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.log("MongoDB connection error:", err);
+  });
+
 
 // home test route 
 app.get ('/test',(req, res)=> {
     res.send("server is working ")
 }); 
+
 
 app.listen(3000, () => {
     console.log("Server reunning on port 3000")
